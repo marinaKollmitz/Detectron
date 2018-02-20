88be29bac34625075c5dfdfdc72481dfc32d670a
Marina Kollmitz <kollmitz@informatik.uni-freiburg.de>
2018-02-20 12:53:10 +0100

    added hospital dataset in catalog

--- lib/datasets/dataset_catalog.py
+++ lib/datasets/dataset_catalog.py
@@ -185,5 +185,17 @@ DATASETS = {
             _DATA_DIR + '/VOC2012/annotations/voc_2012_trainval.json',
         DEVKIT_DIR:
             _DATA_DIR + '/VOC2012/VOCdevkit2012'
+    },
+    'hospital_train': {
+        IM_DIR:
+            _DATA_DIR + '/hospital_people/images',
+        ANN_FN:
+            _DATA_DIR + '/hospital_people/annotations/train.json'
+    },
+    'hospital_test': {
+        IM_DIR:
+            _DATA_DIR + '/hospital_people/images',
+        ANN_FN:
+            _DATA_DIR + '/hospital_people/annotations/test.json'
     }
 }
